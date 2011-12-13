f02dd1f51830e732180115c4c5b481210f24ec6e
Jose Barbosa <jpedrombarbosa@gmail.com>
2011-12-13 18:31:47 +0000

    resolucao do search de forma melhorada

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,23 +3,28 @@ class SearchController < ApplicationController
   skip_before_filter :authorize
   
   def new
+    
+    #@search = Search.find(params[:id])
+    puts "params ---- 111"
+    puts params
     @search = Search.new
-    @search.keywords = session[:keywords]
+    @search.keywords = params[:keywords]
     @series = @search.series.paginate({:page => params[:page], :per_page => 30})
     #redirect_to series_index_path(@search.series)
     
-    render :file  => "series/index", :locals => {:series => @series}
+    render :file  => "series/index", :locals => {:series => @series, :keywords => @search.keywords}
   end
 
   def create
     @search = Search.new
     @search.keywords = params[:keywords]
-    session[:keywords] = params[:keywords]
+    #@search.save
+    #session[:keywords] = params[:keywords]
     
     @series = @search.series.paginate({:page => params[:page], :per_page => 30})
     #redirect_to series_index_path(@search.series)
     
-    render :file  => "series/index", :locals => {:series => @series}
+    render :file  => "series/index", :locals => {:series => @series, :keywords => @search.keywords}
   end
 
   def destroy
